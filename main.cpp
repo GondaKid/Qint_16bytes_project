a4c8d5d552058933e1970e9d7ffe8a572ebbcc23
Gonn <thienphuc16066@gmail.com>
2020-06-05 22:13:34 +0700

    delete some redundancy comments

--- main.cpp
+++ main.cpp
@@ -1,12 +1,5 @@
 #include "./Header/MainLoop.h"
 
-// int main() {
-//     QInt a("1898003864", 10);
-//     cout << (a.rol()).getDec();
-
-//     return 0;
-// }
-
 int main(int argc, char *argv[]) {
     MainLoop M;
     if (argc < 2)
